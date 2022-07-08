2ae8f3c28972b1e2c59f6db8f1f5ddac8e443f43
Dumitru Bogdan <dumitrumihaibogdan@gmail.com>
2022-07-08 13:35:25 +0300

    Use mocked engine.

--- src/test/java/DaciaTest.java
+++ src/test/java/DaciaTest.java
@@ -1,13 +1,20 @@
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
 public class DaciaTest {
   @Test
   public void testSpeed() throws InterruptedException {
-    DaciaEngine engine = new DaciaEngine();
-    Dacia dacia = new Dacia(engine);
-    dacia.setAcceleration(500);
+    DaciaEngine mockedEngine = mock(DaciaEngine.class);
+    mockedEngine.setAcceleration(100);
+    when(mockedEngine.getSpeedIncrement())
+      .thenReturn(1.0);
+    
+    Dacia dacia = new Dacia(mockedEngine);
+    dacia.startEngine();
+    dacia.setAcceleration(100);
     Thread.sleep(4000);
     assertTrue(dacia.getSpeed() > 0);
   }
